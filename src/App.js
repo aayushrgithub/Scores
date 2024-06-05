1f0d20d7496980712a244276295eaabf012debfd
aayushrgithub <110468662+aayushrgithub@users.noreply.github.com>
2024-06-05 17:15:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import newImage from "./images/newImage.jpg"
 function App() {
   return (
     <div>
-      <img className="full" src={newImage}/>
+      <img className="h-[100dvh] w-full" src={newImage}/>
       <div className="absolute w-[100%] h-[100%] top-0 backdrop-blur-sm">
       <Header/>
       <Body/>
